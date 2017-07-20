668a61252e45a81cdc91f95e62dd009d151a5821
SkyASC4 <sinfante@chelseahs.org>
2017-07-20 14:24:55 -0400

    failed projects

---
+++ Sky-pokemonbattle/sketch.js
@@ -0,0 +1,25 @@
+function setup(){
+createCanvas(750, 600);
+background(150)
+}
+
+
+function draw(){
+    rect(0, 399, 399, 200);
+ text("Hey, what's up?", 50, 410, 410, 200)
+ text("")
+    rect(350, 399, 399, 200);
+   
+}
+
+var MyArray = [Charizard, Eevee, Balbasaur, Chamander];
+function setup (name, HP, type, attack ){
+    this.name = name,
+    this.HP =  HP,
+    this.type = type,
+    this.attack = attack
+}
+ "name" ; "Charizard",
+ "HP" ; 100,
+ "type" ; "Fire",
+ "attack" ; 60,
